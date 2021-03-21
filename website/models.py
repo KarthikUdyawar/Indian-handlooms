74e11c8cd045b9152edbe99e3f56698bda577910
Karthik Udyawar <54676225+KarthikUdyawar@users.noreply.github.com>
2021-03-22 01:02:19 +0530

    Added login system
    Creating a login system for weavers

---
+++ website/models.py
@@ -0,0 +1,15 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # TODO add column to profile
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    profile = db.relationship('Profile')
