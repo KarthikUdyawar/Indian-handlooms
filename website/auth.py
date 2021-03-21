c8ab3eb7cf8f83f0a8ebeeab7013e876bc636227
Karthik Udyawar <54676225+KarthikUdyawar@users.noreply.github.com>
2021-03-21 16:50:49 +0530

    Adding login base
    Base of login system

--- website/auth.py
+++ website/auth.py
@@ -3,9 +3,21 @@ from flask import Blueprint, render_template
 auth = Blueprint('auth',__name__)
 
 @auth.route('/about')
-def login():
+def about():
     return render_template("about.html")
 
 @auth.route('/database')
-def sign_up():
+def database():
     return render_template("database.html")
+
+@auth.route('/login')
+def login(): 
+    return render_template("login.html")
+
+@auth.route('/sign-up')
+def sign_up():
+    return render_template("signUp.html")
+
+@auth.route('/logout')
+def logout():
+    return '<h3>Logout</h3>'
