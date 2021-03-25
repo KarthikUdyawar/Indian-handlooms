a823ab34545935d1112b74fcb67449167815e644
Karthik Udyawar <54676225+KarthikUdyawar@users.noreply.github.com>
2021-03-26 02:15:20 +0530

    Changing profile page
    Changing profile page.

--- website/models.py
+++ website/models.py
@@ -4,12 +4,14 @@ from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
+    email = db.Column(db.String(150)) # , unique=True
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     company_Name = db.Column(db.String(150))
-    contact = db.Column(db.String(10), unique=True)
+    contact = db.Column(db.String(10)) # , unique=True
     description = db.Column(db.String(500))
+    state = db.Column(db.String(20))
+    address = db.Column(db.String(200))
     
 # class Profile(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
--- website/views.py
+++ website/views.py
@@ -12,38 +12,52 @@ def profile():
     if request.method == 'POST':
         cName = request.form.get('cName')
         firstName = request.form.get('firstName')
-        email = request.form.get('email')
         contact = request.form.get('contact')
+        state = request.form.get('state')
+        address = request.form.get('address')
         desc = request.form.get('desc')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
+
+        # if cName == '':
+        #     cName = User.company_Name
+        # elif firstName == '':
+        #     firstName = User.first_name
+        # elif contact == '':
+        #     contact = User.contact
+        # elif state == 'None':
+        #     state = User.state
+        # elif address == '':
+        #     address = User.address
+        # elif desc == '':
+        #     desc = User.desc
+        # else:
+        #     pass
         
-        user = User.query.filter_by(email=email).first()
-        if user:
-            flash('Email already exists.', category='error')
-        elif len(email) < 4:
-            flash('Email must be at least 3 characters.',category='error')
-        elif len(firstName) < 2:
-            flash('First name must be at least 2 characters.',category='error')
-        elif password1 != password2:
-            flash('Password does not match.',category='error')
-        elif len(password1) < 7:
-            flash('Password must be at least 7 characters.',category='error')
-        elif len(cName) < 2:
-            flash('First name must be at least 2 characters.',category='error')
-        elif len(contact) != 10:
-            flash('Contact number must be 10 digits.',category='error')
-        else:
+        # user = User.query.filter_by(email=email).first() 
+        # if user and email != user.email:
+        #     flash('Email already exists.', category='error')
+        # elif len(email) < 4 and email != '':
+        #     flash('Email must be at least 3 characters.',category='error')
+        # elif len(firstName) < 2 and firstName != '':
+        #     flash('First name must be at least 2 characters.',category='error')
+        # elif password1 != password2:
+        #     flash('Password does not match.',category='error')
+        # elif len(password1) < 7 and password1 != '' and password2 != '':
+        #     flash('Password must be at least 7 characters.',category='error')
+        # elif len(cName) < 2:
+        #     flash('First name must be at least 2 characters.',category='error')
+        # elif len(contact) != 10 and contact != '':
+        #     flash('Contact number must be 10 digits.',category='error')
+        # else:
             # new_profile = User(company_Name = cName, contact = contact, description=desc)
             # db.session.add(new_profile)
-            user = User.query.get(current_user.id)
-            user.company_Name = cName
-            user.first_name = firstName
-            user.email = email
-            user.contact = contact
-            user.description = desc
-            user.password = generate_password_hash(password1,method='sha256')
-            db.session.commit()
-            flash('Profile edited!.',category='success')
+        user = User.query.get(current_user.id)
+        user.company_Name = cName
+        user.first_name = firstName
+        user.contact = contact
+        user.state = state
+        user.address = address
+        user.description = desc
+        db.session.commit()
+        flash('Profile edited!.',category='success')
         
     return render_template("profile.html", user=current_user)
