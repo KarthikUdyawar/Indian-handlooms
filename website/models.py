577b0abad30386a54bc6a799364943d40a2a2f50
Karthik Udyawar <54676225+KarthikUdyawar@users.noreply.github.com>
2021-04-02 18:00:29 +0530

    Revert "Adding logo"
    This reverts commit 96244641b57413566cfc64536417612812e54ab1.

--- website/models.py
+++ website/models.py
@@ -4,13 +4,27 @@ from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150))
+    email = db.Column(db.String(150)) # , unique=True
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     company_Name = db.Column(db.String(150))
-    contact = db.Column(db.String(10)) 
+    contact = db.Column(db.String(10)) # , unique=True
     description = db.Column(db.String(500))
     state = db.Column(db.String(20))
     address = db.Column(db.String(200))
     image = db.Column(db.String(200))
     product_name = db.Column(db.String(100))
+    
+# class Profile(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     company_Name = db.Column(db.String(150))
+#     contact = db.Column(db.String(10))
+#     description = db.Column(db.String(500))
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+# class User(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(150), unique=True)
+#     password = db.Column(db.String(150))
+#     first_name = db.Column(db.String(150))
+#     profiles = db.relationship('Profile')
--- website/views.py
+++ website/views.py
@@ -2,11 +2,13 @@ from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, login_user, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
-from .models import User 
+from .models import User #, Profile
 from . import db
 import os
+# import re
 
 views = Blueprint('views',__name__)
+# app.config["IMAGE_UPLOADS"] = '\website\static\images\profile'
 
 def allowed_image(filename):
     if not "." in filename:
@@ -26,7 +28,7 @@ def allowed_image_filesize(filesize):
 @views.route('/profile', methods=['GET','POST'])
 @login_required
 def profile():
-    if request.method == 'POST':  
+    if request.method == 'POST':  #  and 'cName' in request.form and 'firstName' in request.form and 'contact' in request.form and 'state' in request.form and 'address' in request.form and 'address' in request.form
         cName = request.form.get('cName')
         firstName = request.form.get('firstName')
         contact = request.form.get('contact')
@@ -40,7 +42,9 @@ def profile():
             if "filesize" in request.cookies:
                 image = request.files["image"]
                 if image.filename == "":
+                    # print("No filename") 
                     path_image = User.image
+                # print(image)
                 elif not allowed_image_filesize(request.cookies["filesize"]):
                     flash('Filesize exceeded maximum limit (500 Kb)', category='error')
                     path_image = User.image
@@ -48,8 +52,11 @@ def profile():
                     filename = secure_filename(image.filename)
                     path_image = os.path.join("website/static/images/profile/" , filename)
                     image.save(path_image)
+                    # print("Image saved")
                 else:
                     flash('That file extension is not allowed', category='error')
+
+        # print(len(contact))
 
         if cName == '':
             cName = User.company_Name
@@ -62,6 +69,12 @@ def profile():
         elif len(firstName) < 3:
             flash('First Name must be at least 3 characters.', category='error')
             firstName = User.first_name
+        
+        # if contact == '':
+        #     contact = User.contact
+        # elif not (re.match('^[0-9]*$', contact)) or len(contact) == 11:                            
+        #     flash('Contact must be at 10 digits.', category='error')
+        #     contact = User.contact
             
         if contact == '':
             contact = User.contact
@@ -89,6 +102,32 @@ def profile():
         elif len(desc) < 3:
             flash('Description must be at least 3 characters.', category='error')
             desc = User.description
+
+        # if len(cName) < 3:
+        #     flash('Company Name must be at least 3 characters.', category='error')
+        # if len(firstName) < 3:
+        #     flash('First Name must be at least 3 characters.', category='error')
+        # if len(contact) < 10 or contact.isdigit():
+        #     flash('Contact must be at 10 digits.', category='error')
+        
+        # user = User.query.filter_by(email=email).first() 
+        # if user and email != user.email:
+        #     flash('Email already exists.', category='error')
+        # elif len(email) < 4 and email != '':
+        #     flash('Email must be at least 3 characters.',category='error')
+        # elif len(firstName) < 2 and firstName != '':
+        #     flash('First name must be at least 2 characters.',category='error')
+        # elif password1 != password2:
+        #     flash('Password does not match.',category='error')
+        # elif len(password1) < 7 and password1 != '' and password2 != '':
+        #     flash('Password must be at least 7 characters.',category='error')
+        # elif len(cName) < 2:
+        #     flash('First name must be at least 2 characters.',category='error')
+        # elif len(contact) != 10 and contact != '':
+        #     flash('Contact number must be 10 digits.',category='error')
+        # else:
+        #     new_profile = User(company_Name = cName, contact = contact, description=desc)
+        #     db.session.add(new_profile)
         
         user = User.query.get(current_user.id)
         user.company_Name = cName
@@ -101,6 +140,9 @@ def profile():
         user.password = current_user.password
         user.image = path_image
         user.product_name = pName
+        
+        # newprofile = User(first_name=firstName, contact=contact, company_Name=cName, state=state, address=address, description=desc, email=current_user.email, password=current_user.password)
+        # db.session.add(newprofile)
         
         db.session.commit()
         flash('Profile edited!',category='success')
